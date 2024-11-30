7f762ad25a4fb70a50b2b45c1c685ad8b6336b74
Abdur Rakib <89575718+Hsjeksjsy68@users.noreply.github.com>
2024-11-30 22:02:51 +0600

    Update match-data.js

--- match-data.js
+++ match-data.js
@@ -1,15 +1,15 @@
 document.getElementById("box-1-ffc").innerText = 'FFC';
-document.getElementById("box-1-clubName").innerText = 'FFC';
-document.getElementById("box-1-time").innerText = 'FFC';
-document.getElementById("box-1-address").innerText = 'FFC';
+document.getElementById("box-1-clubName").innerText = 'RTALIATION FC';
+document.getElementById("box-1-time").innerText = '8 AM AT,';
+document.getElementById("box-1-address").innerText = 'AFTAB NAGAR, SOUTH BANASREE, DHAKA - 1219';
 
 
 document.getElementById("box-2-ffc").innerText = 'FFC';
-document.getElementById("box-2-clubName").innerText = 'FFC';
-document.getElementById("box-2-time").innerText = 'FFC';
-document.getElementById("box-2-address").innerText = 'FFC';
+document.getElementById("box-2-clubName").innerText = 'NOT FIXED';
+document.getElementById("box-2-time").innerText = 'NOT SURE AT,';
+document.getElementById("box-2-address").innerText = 'NOT SURE';
 
 document.getElementById("box-3-ffc").innerText = 'FFC';
-document.getElementById("box-3-clubName").innerText = 'FFC';
-document.getElementById("box-3-time").innerText = 'FFC';
-document.getElementById("box-3-address").innerText = 'FFC';
+document.getElementById("box-3-clubName").innerText = 'NOT FIXED';
+document.getElementById("box-3-time").innerText = 'NOT SURE AT,';
+document.getElementById("box-3-address").innerText = 'NOT SURE';
