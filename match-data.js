70029107ce2a5d4f15dd0297213d28ef36feedf3
Abdur Rakib <89575718+Hsjeksjsy68@users.noreply.github.com>
2024-11-30 21:58:49 +0600

    Add files via upload

---
+++ match-data.js
@@ -0,0 +1,15 @@
+document.getElementById("box-1-ffc").innerText = 'FFC';
+document.getElementById("box-1-clubName").innerText = 'FFC';
+document.getElementById("box-1-time").innerText = 'FFC';
+document.getElementById("box-1-address").innerText = 'FFC';
+
+
+document.getElementById("box-2-ffc").innerText = 'FFC';
+document.getElementById("box-2-clubName").innerText = 'FFC';
+document.getElementById("box-2-time").innerText = 'FFC';
+document.getElementById("box-2-address").innerText = 'FFC';
+
+document.getElementById("box-3-ffc").innerText = 'FFC';
+document.getElementById("box-3-clubName").innerText = 'FFC';
+document.getElementById("box-3-time").innerText = 'FFC';
+document.getElementById("box-3-address").innerText = 'FFC';
